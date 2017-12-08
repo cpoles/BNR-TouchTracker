4804cd3f2c738efa8b8abcbc4315fb65c77ac966
cpoles <carlos.poles@griffithuni.edu.au>
2017-12-08 17:37:53 +1000

    Added Line.swift and created Line struct

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Carlos Poles on 8/12/17.
+//  Copyright © 2017 Carlos Poles. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
